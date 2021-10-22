e52631b4ffb370ecfc2744886c3e12091d7bddc1
AhmadMCer <ahmadmcer@gmail.com>
2021-10-22 22:05:16 +0700

    FIX: Delete unnecessary code in script.js

--- script.js
+++ script.js
@@ -11,36 +11,3 @@ function dropMenu() {
     hidden = true;
   }
 }
-
-
-let i = 5
-
-const animation = (i) => {
-  let j = i % 5
-  switch (j) {
-    case 0:
-      document.getElementById('title').innerHTML = 'Remember the Legendary...'
-      break
-    case 1:
-      document.getElementById('title').style.margin = '91.5px 0 0 0'
-      document.getElementById('title').innerHTML = ''
-      document.getElementById('image').src = 'goodbye_coco.jpeg'
-      break
-    case 2:
-      document.getElementById('title').style.margin = '335.5px 0 0 0'
-      document.getElementById('title').innerHTML = '#GoodbyeCoco'
-      document.getElementById('image').src = ''
-      break
-    case 3:
-      document.getElementById('title').innerHTML = '<i>Made with HTML, CSS, JavaScript</i>'
-      break
-    case 4:
-      document.getElementById('title').innerHTML = 'Hello World!'
-      break
-  }
-}
-
-setInterval(() => {
-  animation(i)
-  i++
-}, 2500)
